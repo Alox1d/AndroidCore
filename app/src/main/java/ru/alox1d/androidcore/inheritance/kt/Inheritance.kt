b1ce3d6c23a7d7f756a1035d0dd69d27a88a4d43
Alox1d <alox1dab@gmail.com>
2024-01-29 22:56:41 +0300

    added polymorphism in inheritance example

--- app/src/main/java/ru/alox1d/androidcore/inheritance/kt/Inheritance.kt
+++ app/src/main/java/ru/alox1d/androidcore/inheritance/kt/Inheritance.kt
@@ -1,13 +1,22 @@
 package inheritance.kt
 
 open class B {
-    open val s1 = "s1"
-    fun foo() {
+    init {
+        println("init b")
+    }
 
+    open val s1 = "s1"
+    open fun foo() {
+        println("B")
     }
 }
 
 class A : B() {
+
+    init {
+        println("init a")
+    }
+
     override val s1 = "s2"
 
     init {
@@ -15,7 +24,11 @@ class A : B() {
     }
 
     fun foo(s2: String) {
+        println(s2)
+    }
 
+    override fun foo() {
+        println("A")
     }
 }
 
@@ -24,4 +37,7 @@ fun main() {
     val a = A()
     a.foo() // A has base method foo() too
     a.foo("Overloaded")
+
+    val var_A_as_Parent_B: B = A()
+    var_A_as_Parent_B.foo()
 }
