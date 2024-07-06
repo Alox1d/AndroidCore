3ce959a69cab2a5a8ffa40f05688cfbf258c24d6
Alox1d <alox1dab@gmail.com>
2024-07-06 22:25:49 +0300

    added Nothing example + string format

---
+++ app/src/main/java/ru/alox1d/androidcore/nothing/checkAge.kt
@@ -0,0 +1,14 @@
+package ru.alox1d.androidcore.nothing
+
+fun failWithWrongAge(age: Int?): Nothing { // compile error without Nothing
+    throw IllegalArgumentException("Wrong age: $age")
+}
+
+fun checkAge(age: Int?) {
+    if (age == null || age !in 0..150) failWithWrongAge(age)
+    println("Congrats! Next year you'll be ${age + 1}.")
+}
+
+fun main() {
+    checkAge(10)
+}
--- app/src/main/java/ru/alox1d/androidcore/string/SimpleString.kt
+++ app/src/main/java/ru/alox1d/androidcore/string/SimpleString.kt
@@ -2,6 +2,8 @@ package string
 
 fun main() {
     foo(1.toString())
+    val rawText = "Hello string".format("string")
+    println(rawText)
 }
 
 fun foo(s: String) {
